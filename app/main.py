6ab3e7286484e966fc27a0b19dddb6f830f61891
CarlosMoyanoG <carlosmoyanoguevara@gmail.com>
2026-01-29 17:38:28 -0500

    Se mapean correctamente los n ombres de variables para no tener que nombrarlas distinto en front ya que se tiene logica prehecha

--- app/main.py
+++ app/main.py
@@ -1,31 +1,40 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models, schemas, database, service
 
 models.Base.metadata.create_all(bind=database.engine)
 app = FastAPI(title="Usuarios API - FastAPI")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 def get_usuario_service(db: Session = Depends(database.get_db)):
     return service.UsuarioService(db)
 
-@app.get("/api/usuarios", response_model=List[schemas.Usuario])
+@app.get("/api/usuarios", response_model=List[schemas.UsuarioOut])
 def read_usuarios(service: service.UsuarioService = Depends(get_usuario_service)):
     return service.get_all()
 
-@app.get("/api/usuarios/{usuario_id}", response_model=schemas.Usuario)
+@app.get("/api/usuarios/{usuario_id}", response_model=schemas.UsuarioOut)
 def read_usuario(usuario_id: int, service: service.UsuarioService = Depends(get_usuario_service)):
     usuario = service.get_by_id(usuario_id)
     if usuario is None:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return usuario
 
-@app.post("/api/usuarios", response_model=schemas.Usuario, status_code=status.HTTP_201_CREATED)
+@app.post("/api/usuarios", response_model=schemas.UsuarioOut, status_code=status.HTTP_201_CREATED)
 def create_usuario(usuario: schemas.UsuarioCreate, service: service.UsuarioService = Depends(get_usuario_service)):
     return service.create(usuario)
 
-@app.put("/api/usuarios/{usuario_id}", response_model=schemas.Usuario)
+@app.put("/api/usuarios/{usuario_id}", response_model=schemas.UsuarioOut)
 def update_usuario(usuario_id: int, usuario: schemas.UsuarioUpdate, service: service.UsuarioService = Depends(get_usuario_service)):
     db_usuario = service.update(usuario_id, usuario)
     if db_usuario is None:
